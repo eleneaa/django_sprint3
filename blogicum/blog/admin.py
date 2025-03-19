d653fc1cf8c1b314114fc1a1a91159e1e4aa30cd
eleneaa <lenatisenko4@gmail.com>
2025-03-19 17:01:29 +0500

    done maybe

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from django_sprint3.blogicum.blog.models import Post, Category, Location
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Location)
+
+# Register your models here.
---
+++ blogicum/blog/views.py
@@ -0,0 +1,47 @@
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+from django_sprint3.blogicum.blog.models import Post, Category
+
+
+def index(request):
+    post_list = Post.objects.filter(
+        pub_date__lte=timezone.now(),
+        is_published=True,
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+
+    context = {
+        'post_list': post_list
+    }
+    return render(request, "blog/index.html", context)
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, pk=id, is_published=True)
+
+    if post.pub_date > timezone.now() or not post.category.is_published:
+        raise Http404("Публикация недоступна.")
+
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/detail.html', context)
+
+
+def category_posts(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug, is_published=True)
+
+    post_list = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=timezone.now()
+    )
+
+    context = {
+        'category': category,
+        'post_list': post_list
+    }
+    return render(request, 'blog/category.html', context)
+
